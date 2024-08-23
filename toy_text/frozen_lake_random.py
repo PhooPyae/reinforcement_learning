148de05d522abe6ac461ad5c4c58546e93dead9c
PhooPyae <linnphoopyaepyae@gmail.com>
2024-08-23 15:02:13 -0700

    frozen lake random

---
+++ toy_text/frozen_lake_random.py
@@ -0,0 +1,29 @@
+import gymnasium as gym
+from gymnasium.wrappers import RecordVideo
+import logging
+from combine_video import combine_videos
+import json
+
+logging.basicConfig(level=logging.WARNING)
+
+action_map = ['Left', 'Down', 'Right', 'Up']
+show_action = False
+
+if __name__ == '__main__':
+    env = gym.make("FrozenLake-v1", is_slippery=False)
+    episodes = 1000
+    output = []
+    reward_over_episode = 0
+    for episode in range(episodes):
+        obs, info = env.reset()
+        total_reward = 0
+        done = False
+        while not done:
+            action = env.action_space.sample()
+            if show_action:
+                print(f'Action {action_map[action]}')
+            obs_, reward, done, info, _ = env.step(action)
+            total_reward += reward
+        print(f'episode {episode}, total reward {total_reward:.1f}')
+        reward_over_episode += reward
+    print(f'Average Reward: {reward_over_episode/episodes}')
