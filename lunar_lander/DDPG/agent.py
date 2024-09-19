271e950aba331cd6deebe4a032e0b66bf200359a
PhooPyae <linnphoopyaepyae@gmail.com>
2024-09-19 14:16:58 -0700

    add device

--- lunar_lander/DDPG/agent.py
+++ lunar_lander/DDPG/agent.py
@@ -22,34 +22,38 @@ class Agent:
         self.replay_buffer = ReplayBuffer(config.max_size, state_space, action_space)
 
         self.noise = OUActionNoise(mu=np.zeros(action_space))
-        
-        self.actor_network = Actor(state_space, action_space)
-        self.critic_network = Critic(state_space, action_space)
 
-        self.actor_target_network = Actor(state_space, action_space)
-        self.critic_target_network = Critic(state_space, action_space)
+        self.actor_network = Actor(state_space, action_space).to(self.device)
+        self.critic_network = Critic(state_space, action_space).to(self.device)
+
+        self.actor_target_network = Actor(state_space, action_space).to(self.device)
+        self.critic_target_network = Critic(state_space, action_space).to(self.device)
 
         self.actor_optimizer = optim.Adam(self.actor_network.parameters(), lr=self.alpha)
         self.critic_optimizer = optim.Adam(self.critic_network.parameters(), lr=self.beta)
 
-    
+        self.noise_value = None
+
+
     def choose_action(self, state):
         self.actor_network.eval()
-        
+
         state = torch.tensor(state, dtype=torch.float32).to(self.device)
         noise = torch.tensor(self.noise(), dtype=torch.float32).to(self.device)
         mu = self.actor_network(state)
         mu_prime = mu + noise
-        
+        self.noise_value = noise.cpu().detach().numpy()
+
         self.actor_network.train()
+
         return mu_prime.cpu().detach().numpy()
-    
+
     def learn(self):
         if self.replay_buffer.mem_cntr < self.batch_size:
             return None, None
 
         states, actions, rewards, states_, dones = self.replay_buffer.sample_buffer(self.batch_size)
-        
+
         states = torch.tensor(states, dtype=torch.float32).to(self.device)
         actions = torch.tensor(actions, dtype=torch.float32).to(self.device)
         rewards = torch.tensor(rewards, dtype=torch.float32).to(self.device)
@@ -59,7 +63,7 @@ class Agent:
         # y = r + gamma * Q_prime(s_, mu_prime(s_,))
         mu_ = self.actor_target_network(states_)
         critic_value_ = self.critic_target_network(states_, mu_)
-        
+
         #if done, 0
         critic_value_[dones] = 0.0
         critic_value_ = critic_value_.view(-1)
@@ -69,7 +73,7 @@ class Agent:
         target = rewards + self.gamma * critic_value_
         target = target.view(self.batch_size, 1)
 
-        critic_loss = F.mse_loss(critic_value, target) #(batch_size, 1)
+        critic_loss = F.mse_loss(critic_value, target)
 
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
@@ -86,6 +90,7 @@ class Agent:
         self.update_target_network()
 
         return critic_loss.item(), actor_loss.item()
+
 
     def update_target_network(self, tau=None):
         if tau is None:
@@ -111,7 +116,3 @@ class Agent:
 
         self.critic_target_network.load_state_dict(critic_state_dict)
         self.actor_target_network.load_state_dict(actor_state_dict)
-    
-    
-    
-    
