e247872a766d4790d082cf629586504c995c469e
PhooPyae <linnphoopyaepyae@gmail.com>
2024-09-20 15:05:38 -0700

    add logs

--- multi-agents/connect_four/main.py
+++ multi-agents/connect_four/main.py
@@ -1,10 +1,42 @@
 import torch
 import copy
+import wandb
+import numpy as np
+import random
 
 from env_wrapper import SingleAgentSelfPlayEnv
 from agent import Agent
 from utils import evaluate_agent
+import config as Config
 from torch.utils.tensorboard import SummaryWriter
+import time
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+config = Config.config
+
+wandb_project_name = 'connect_four'
+run_name = f"ppo__{config["seed"]}__{int(time.time())}"
+    
+wandb.init(
+            project=wandb_project_name,
+            sync_tensorboard=True,
+            config=config,
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+
+writer = SummaryWriter(f"runs/{run_name}")
+writer.add_text(
+    "hyperparameters",
+    "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in config.items()])),
+)
+
+random.seed(config["seed"])
+np.random.seed(config["seed"])
+torch.manual_seed(config["seed"])
 
 if __name__ == '__main__':
     writer = SummaryWriter()
@@ -28,7 +60,7 @@ if __name__ == '__main__':
             action_mask = obs['action_mask']
 
             action, prob, entropy, value = agent_policy.choose_action(state, action_mask)
-            # print(f'action {action}')
+            # logger.info(f'action {action}')
             next_obs, reward, done, info = env_test.step(action)
 
             agent_policy.memory.store_memory(state, action, prob,  value.item(), reward, done)
@@ -46,19 +78,28 @@ if __name__ == '__main__':
         writer.add_scalar('Loss/entropy', entropy, global_step)
         writer.add_scalar('Loss/value', value, global_step)
         writer.add_scalar('Loss/prob', prob, global_step)
+        writer.add_scalar("charts/episodic_return", total_reward, global_step)
+        writer.add_scalar("charts/episodic_length", step, global_step)
 
 
+        logger.info(f"global_step={global_step}, episodic_return={total_reward}")
         
         if episode % opponent_update_interval == 0:
             opponent_policy = copy.deepcopy(agent_policy)
             env_test.set_policies(agent_policy=agent_policy, opponent_policy=opponent_policy)
-            print(f'Episode {episode}: Total Reward: {total_reward}: Actor loss: {actor_loss:.2f}, Critic loss: {critic_loss:.2f}, Total loss: {total_loss:.2f}')
+            logger.info(f'Episode {episode}: Total Reward: {total_reward}: Actor loss: {actor_loss:.2f}, Critic loss: {critic_loss:.2f}, Total loss: {total_loss:.2f}')
 
             # #player 1
-            evaluate_agent(agent_policy=agent_policy, opponent_policy=opponent_policy, agent_starts=True, num_episodes=10)
+            win_rate, draw_rate, loss_rate = evaluate_agent(agent_policy=agent_policy, opponent_policy=opponent_policy, agent_starts=True, num_episodes=10)
+            writer.add_scalar("evaluate/player1_win_rate", win_rate, global_step)
+            writer.add_scalar("evaluate/player1_draw_rate", draw_rate, global_step)
+            writer.add_scalar("evaluate/player1_loss_rate", loss_rate, global_step)
             # #player 2
             evaluate_agent(agent_policy=agent_policy, opponent_policy=opponent_policy, agent_starts=False, num_episodes=10)
-
-            print(f'========')
+            writer.add_scalar("evaluate/player2_win_rate", win_rate, global_step)
+            writer.add_scalar("evaluate/player2_draw_rate", draw_rate, global_step)
+            writer.add_scalar("evaluate/player2_loss_rate", loss_rate, global_step)
+            
+            logger.info(f'========')
             torch.save(agent_policy.actor_critic_network.state_dict(), 'agent_policy_network.pth')
             
